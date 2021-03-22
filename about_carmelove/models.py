50996265695f5d0a27f7f017315b67bfedf571f4
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-03-22 23:32:04 +0100

    feature(cl): implement model, register in admin and add model into view

--- about_carmelove/models.py
+++ about_carmelove/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+
+class Article(Model):
+    title = CharField(max_length=70, null=False, blank=False)
+    preface = TextField(max_length=500, null=True, blank=True)
+    text = TextField(max_length=3000, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
